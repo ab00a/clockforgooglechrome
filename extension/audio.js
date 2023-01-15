727cabc66d2c45ac8b31c24030541604a3c714c7
ab00a <ab00a@example.com>
2023-01-15 17:33:12 +0000

    Updated BootStrap

--- extension/audio.js
+++ extension/audio.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.type == "soundAudio") {
-        sendResponse({msg, reply: "Request for sound received"});
+        sendResponse({ msg, reply: "Request for sound received" });
         var audio = new Audio("assets/" + msg.audioSrc); // Add register and error checking logic
         audio.id = msg.id;
         audio.loop = true; // Alarms just keep going
@@ -8,10 +8,16 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
             audio.volume = msg.volume;
         }
         audio.play();
+        audio.onpause = function () {
+            if (audio.ended) {
+                audio.remove();
+                audio = null;
+            };
+        };
     }
 
     if (msg.type == "chime") {
-        sendResponse({msg, reply: "Request for chime received"});
+        sendResponse({ msg, reply: "Request for chime received" });
         var audio = new Audio("assets/diing.ogg"); // Add register and error checking logic
         audio.id = "chime";
         audio.loop = false; // Chime rings once
@@ -28,13 +34,11 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     }
 
     if (msg.type == "stopAudio") {
-        sendResponse({msg, reply: "Request for stop received"});
+        sendResponse({ msg, reply: "Request for stop received" });
         var audio = document.getElementById(msg.id);
         if (audio != null) {
             audio.pause();
-            audio.remove();
-            audio = null;
         }
     }
-    
-  });
+
+});
